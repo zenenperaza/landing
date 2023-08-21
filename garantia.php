d56e9dff576e9d29610cbfca989159dc862abe7e
Zenen Peraza <peraza@outlook.com>
2023-08-21 13:27:56 -0400

    GARANTIA

---
+++ garantia.php
@@ -0,0 +1,107 @@
+<?php
+    include('./inclusiones/head.php');
+?>
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <?php
+            include('./inclusiones/header.php');
+        ?>
+        <!-- end: Header -->
+
+        <div class="container-fullscreen">
+            <div class="text-middle">
+                <div class="row">
+                <div class="heading-text heading-section text-center">
+            <h2>GARANTIA</h2>
+        </div>
+        <section class="">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-6">
+                        <h2>Rounded</h2>
+                        <p>Add classes to an <code>&lt;img&gt;</code> element to easily style images in any project.</p>
+                        <img src="images/other/550x240.png" class="img-fluid rounded" alt="">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="heading-text heading-section">
+                            <h2>THE COMPANY</h2>
+                            <span class="lead">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="background-grey p-t-40 p-b-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-gift"></i></a>
+                            </div>
+                            <h3>Free shipping on orders $60+</h3>
+                            <p>Order more than 60$ and you will get free shippining Worldwide. More info.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-plane"></i></a>
+                            </div>
+                            <h3>Worldwide delivery</h3>
+                            <p>We deliver to the following countries: USA, Canada, Europe, Australia</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-history"></i></a>
+                            </div>
+                            <h3>60 days money back guranty!</h3>
+                            <p>Not happy with our product, feel free to return it, we will refund 100% your money!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="heading-text heading-section">
+                        <h2>TIEMPO</h2>
+                        <span class="lead">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </span>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                <div class="heading-text heading-section">
+                        <h2>CALIDAD</h2>
+                        <span class="lead">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </span>
+                </div>
+                </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-lg-6">
+                        <div class="heading-text heading-section">
+                            <h2>TERMINOS</h2>
+                            <span class="lead">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <p>Add classes to an <code>&lt;img&gt;</code> element to easily style images in any project.</p>
+                        <img src="images/other/550x240.png" class="img-fluid rounded" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Footer -->
+<?php
+    include('./inclusiones/footer.php')
+?>
