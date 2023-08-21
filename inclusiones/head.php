b82d3fd3b2929de08e61cb34650537b37c31fba1
Zenen Peraza <peraza@outlook.com>
2023-08-21 12:00:51 -0400

    first commit

---
+++ inclusiones/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="./">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="INSPIRO" />
+    <meta name="description" content="Themeforest Template Polo, html template">
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>Landing Page</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/plugins.css" rel="stylesheet">
+</head>
---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+    include('./inclusiones/head.php');
+?>
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <?php
+            include('./inclusiones/header.php');
+        ?>
+        <!-- end: Header -->
+
+      
+
+
+        <section class="fullscreen" >
+            <div class="bg-overlay"></div>
+            <div class="container-wide">
+                <div class="container-fullscreen">
+                    <div class="text-middle">
+                        <div class="row">
+                        <div class="heading-text heading-section text-center">
+                    <h2>SERVICIOS</h2>
+                </div>
+                        </div>
+                        <div class="row justify-content-between">
+                            <div class="col-md-5">
+                                <div class="heading-text  ">
+                                    <h5 class="text-uppercase">Features
+                                    </h5>
+                                        <p>Tons of features that will give you the power to create any website</p>
+                                        <p>Tons of features that will give you the power to create any website</p>
+                                        <p>Tons of features that will give you the power to create any website</p>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div class="shadow ml-5">
+                                    <video playsinline="true" autoplay controls loop="true" muted="true" preload="auto">
+                                        <source src="video/pexels-workout.mp4" type="video/mp4"></video>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end: Fullscreen HTML5 video -->
+                <!-- SERVICES -->
+        <section>
+        <div class="grid-layout grid-4-columns testimonial grid-loaded" data-="" data-item="grid-item" style="margin: 0px -20px -20px 0px; position: relative; height: 736px;">
+                    <div class="grid-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 0px; top: 0px;">
+                        <!-- Testimonials item -->
+                        <div class="testimonial-item">
+                            <img src="images/team/6.jpg" alt="">
+                            <p>Polo is by far the most amazing template out there! I literally could not be happier that I chose to buy this template!</p>
+                            <span>Alan Monre</span>
+                            <span>CEO, Square Software.</span>
+                        </div>
+                        <!-- end: Testimonials item-->
+                    </div>
+                    <div class="grid-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 377.328px; top: 0px;">
+                        <!-- Testimonials item -->
+                        <div class="testimonial-item">
+                            <img src="images/team/7.jpg" alt="">
+                            <p>Polo is by far the most amazing template out there! I literally could not be happier that I chose to buy this template!</p>
+                            <span>Resa Smith</span>
+                            <span>Developer @Apple.inc</span>
+                        </div>
+                        <!-- end: Testimonials item-->
+                    </div>
+                    <div class="grid-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 754.656px; top: 0px;">
+                        <!-- Testimonials item -->
+                        <div class="testimonial-item">
+                            <img src="images/team/8.jpg" alt="">
+                            <p>Polo is by far the most amazing template out there! I literally could not be happier that I chose to buy this template!</p>
+                            <span>Alan Monre</span>
+                            <span>CEO, Square Software.</span>
+                        </div>
+                        <!-- end: Testimonials item-->
+                    </div>
+                    <div class="grid-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 0px; top: 368px;">
+                        <!-- Testimonials item -->
+                        <div class="testimonial-item">
+                            <img src="images/team/9.jpg" alt="">
+                            <p>Polo is by far the most amazing template out there! I literally could not be happier that I chose to buy this template!</p>
+                            <span>Resa Smith</span>
+                            <span>Developer @Apple.inc</span>
+                        </div>
+                        <!-- end: Testimonials item-->
+                    </div>
+        </section>
+        <!-- end: SERVICES -->
+
+        <section class="background-grey p-t-40 p-b-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-gift"></i></a>
+                            </div>
+                            <h3>Free shipping on orders $60+</h3>
+                            <p>Order more than 60$ and you will get free shippining Worldwide. More info.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-plane"></i></a>
+                            </div>
+                            <h3>Worldwide delivery</h3>
+                            <p>We deliver to the following countries: USA, Canada, Europe, Australia</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="icon-box effect small clean">
+                            <div class="icon">
+                                <a href="#"><i class="fa fa-history"></i></a>
+                            </div>
+                            <h3>60 days money back guranty!</h3>
+                            <p>Not happy with our product, feel free to return it, we will refund 100% your money!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer -->
+<?php
+    include('./inclusiones/footer.php')
+?>
