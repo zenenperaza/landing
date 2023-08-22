b9da95b4f9ff4d020e26e1af57375bf03edaf954
Zenen Peraza <peraza@outlook.com>
2023-08-21 20:08:09 -0400

    seccion

---
+++ seccion1.php
@@ -0,0 +1,148 @@
+<?php
+    include('./inclusiones/head.php');
+?>
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <?php
+            include('./inclusiones/header.php');
+        ?>
+        <style>
+            .left .post-thumbnail-entry {
+                display: flex;
+                flex-direction: row-reverse;
+            }
+            .left .post-thumbnail-content {
+                padding: 0px!important;
+            }
+            .team-image img {
+                max-width: 100%;
+                height: 235px;
+                margin-bottom: 20px;
+            }
+            .align-self-center.text-align-last-center {
+                text-align-last: center;
+            }
+            .post-thumbnail-list.left {
+                display: flex;
+                flex-direction: column;
+            }
+        </style>
+        <!-- end: Header -->
+  
+        <section id="page-content" >
+            <div class="container">
+                <div class="page-title text-center">
+                    <h1 class="text-uppercase text-medium">ABOUT US</h1>
+                    <span>Work is easy when you have all tools around you!</span>
+                </div>
+            </div>
+        </section>
+
+
+                
+<div class="container">
+                <div class="row">
+                    <dvi class="col-2"></dvi>
+                    <div class="col-lg-6 ">                       
+                        <p class="">This is a simple hero unit, a simple call-to-action-style component for calling extra attention to featured content.</p>
+                    </div>
+                    <div class="col-lg-4 align-self-center text-align-last-center"> <a class="btn btn-primary ">Call us now!</a> </div>
+                </div>
+</div>
+
+        <section id="page-content" >
+
+
+            <div class="container">
+                <div class="row justify-content-center ">
+
+                    
+                            <!-- Sidebar-->
+                            <div class=" col-lg-4">
+                                <!--Tabs with Posts-->
+                                <div class="widget ">
+                                    <div class="post-thumbnail-list left">
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/5.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">A true story, that never been told!</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 6m ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Technology</span>
+                                            </div>
+                                        </div>
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/6.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">Beautiful nature, and rare feathers!</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 24h ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Lifestyle</span>
+                                            </div>
+                                        </div>
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/7.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">Lorem ipsum dolor sit amet</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 11h ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Lifestyle</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--End: Tabs with Posts-->
+                             
+                            </div>
+                            <!-- end: Sidebar-->                    <!-- Sidebar-->
+                            <div class=" col-lg-4">
+                            <div class="team-image">
+                                <img src="images/team/9.jpg">
+                            </div>
+                          
+                            </div>
+                            <!-- end: Sidebar-->                    <!-- Sidebar-->
+                            <div class=" col-lg-4">
+                                <!--Tabs with Posts-->
+                                <div class="widget ">
+                                    <div class="post-thumbnail-list">
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/5.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">A true story, that never been told!</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 6m ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Technology</span>
+                                            </div>
+                                        </div>
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/6.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">Beautiful nature, and rare feathers!</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 24h ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Lifestyle</span>
+                                            </div>
+                                        </div>
+                                        <div class="post-thumbnail-entry">
+                                            <img alt="" src="images/blog/thumbnail/7.jpg">
+                                            <div class="post-thumbnail-content">
+                                                <a href="#">Lorem ipsum dolor sit amet</a>
+                                                <span class="post-date"><i class="icon-clock"></i> 11h ago</span>
+                                                <span class="post-category"><i class="fa fa-tag"></i> Lifestyle</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--End: Tabs with Posts-->
+                              
+                                <!--end: widget newsletter-->
+                            </div>
+                            <!-- end: Sidebar-->
+                </div>
+            </div>
+
+        </section>
+
+
+
+<?php
+    include('./inclusiones/footer.php')
+?>
---
+++ template.php
@@ -0,0 +1,26 @@
+<?php
+    include('./inclusiones/head.php');
+?>
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <?php
+            include('./inclusiones/header.php');
+        ?>
+        <!-- end: Header -->
+  
+        <section id="page-content" >
+            <div class="container">
+                <div class="page-title text-center">
+                    <h1 class="text-uppercase text-medium">ABOUT US</h1>
+                    <span>Work is easy when you have all tools around you!</span>
+                </div>
+            </div>
+        </section>
+      
+
+
+<?php
+    include('./inclusiones/footer.php')
+?>
